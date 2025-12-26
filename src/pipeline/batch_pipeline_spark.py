f17c3a8d4a3ab8641a9dfd9d2603b130d7911c0d
Divin Hegde <divinmr@gmail.com>
2025-12-25 22:53:33 -0600

    Config setup for batch ingestion framework/meta data driven ingestion

--- src/pipeline/batch_pipeline_spark.py
+++ src/pipeline/batch_pipeline_spark.py
@@ -4,25 +4,30 @@ from readers.json_reader_spark import read_json
 from validation.schema_validator_spark import validate_schema
 from validation.dq_validator import apply_dq_rules
 from writers.parquet_writer_spark import write_parquet
+from utils.config_loader import load_config
 
-
+config = load_config("configs/pipeline.yml")
 
 spark = SparkSession.builder \
-    .appName("UnifiedBatchIngestion") \
+    .appName(config["spark"]["app_name"]) \
     .getOrCreate()
 
-input_path = "data/input/binance_events.json"
+df = read_json( spark,config["input"]["path"])
+
+with open(config["schema"]["path"]) as f:
+    expected_schema = json.load(f)
+    
 bronze_good_path = "data/output/bronze/good"
 bronze_bad_path = "data/output/bronze/bad"
 
-with open("schemas/transaction_schema.json") as f:
-    expected_schema = json.load(f)
 
-df = read_json(spark,input_path)
+
 schema_good_df, schema_bad_df = validate_schema(df, expected_schema)
 dq_good_df, dq_bad_df = apply_dq_rules(schema_good_df)
 final_bad_df = schema_bad_df.unionByName(dq_bad_df, allowMissingColumns=True)
 
 # Write outputs
-write_parquet(dq_good_df, bronze_good_path)
-write_parquet(final_bad_df, bronze_bad_path)
+write_parquet(dq_good_df, config["output"]["bronze"]["good"])
+write_parquet(final_bad_df,config["output"]["bronze"]["bad"])
+
+spark.stop()
