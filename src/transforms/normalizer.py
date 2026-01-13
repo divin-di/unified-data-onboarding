1fdd7774b859275c0c0fc9325425fe1bbc9582b6
divin-di <divinmr@gmail.com>
2026-01-13 03:51:59 +0000

    removed duplicate scripts

--- src/transforms/normalizer.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql.functions import col
-
-def normalize(df, mappings):
-    for src, target in mappings.items():
-        if src in df.columns:
-            df = df.withColumnRenamed(src, target)
-    return df
